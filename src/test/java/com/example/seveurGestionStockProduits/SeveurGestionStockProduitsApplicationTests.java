00dad19a8878aec936b92a594be3b33162ee51b2
mathurinfeze <feze.mathurin@gmail.com>
2020-04-10 16:55:17 +0100

    first commit

---
+++ src/test/java/com/example/seveurGestionStockProduits/SeveurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seveurGestionStockProduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeveurGestionStockProduitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
